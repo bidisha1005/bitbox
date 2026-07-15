b2b7ac5d21cfa7c24377ee9bec7c96222962dfcb
bidisha <bidisha.paul10@gmail.com>
2026-07-15 21:43:05 +0530

    fix example hash output in example field

--- tools/sha256_hash.py
+++ tools/sha256_hash.py
@@ -1,7 +1,7 @@
 # tool: sha256_hash
 # description: Computes the SHA-256 hex digest of a string
 # author: @bidisha1005
-# example: sha256_hash "hello" -> "2cf24dba5fb0a30e26e83b2ac5b9e29e1618c90837c5534c2e2ab4f55e16b1e"
+# example: sha256_hash "hello" -> "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
 
 import hashlib
 
